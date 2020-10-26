c027cb237f46c992719a14bd2252a76046a31e14
Andrey Sablin <void.st@gmail.com>
2020-10-26 13:23:21 +0300

    init1

--- src/main/java/org/orion/koffe/repository/impl/CoffeeImpl.java
+++ src/main/java/org/orion/koffe/repository/impl/CoffeeImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CoffeeImpl implements CoffeeRepo {
 
+
   private final CoffeeJpa repo;
 
   public CoffeeImpl(CoffeeJpa repo) {
